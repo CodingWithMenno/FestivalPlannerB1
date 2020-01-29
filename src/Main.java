812a1b2b693110b7b43464ac6e5d30f64d5ed718
Menno <my.bil@student.avans.nl>
2020-01-29 11:51:07 +0100

    Voorbereiding

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        System.out.println("hoi");
+        System.out.println("doeg");
+        //hallo
+    }
+
+
+    public static void main(String[] args) {
+        launch(Main.class);
+    }
+}
