45fbdca5197d91efc75b2e2ca17bc66a4bb9da88
Guilliam Lutz <guilliamlutz@outlook.com>
2020-02-03 19:13:26 +0100

    Button ussage added

--- src/festivalPlanner/data_system/Artist.java
+++ src/festivalPlanner/data_system/Artist.java
@@ -10,4 +10,20 @@ public class Artist {
     private ImageView profilePhoto;
     private String description;
 
+    public Artist(String name, String genre, String description) {
+        this.name = name;
+        this.genre = genre;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", genre='" + genre + '\'' +
+                ", profilePhoto=" + profilePhoto +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
--- src/festivalPlanner/gui/gui_controllers/ArtistScreenController.java
+++ src/festivalPlanner/gui/gui_controllers/ArtistScreenController.java
@@ -1,16 +1,55 @@
 package festivalPlanner.gui.gui_controllers;
 
+import festivalPlanner.data_system.Artist;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.Button;
+
+import java.util.ArrayList;
 
 public class ArtistScreenController {
     @FXML
     private TextField ArtistName;
+    @FXML
+    private TextField ArtistGenre;
+    @FXML
+    private TextArea ArtistInfo;
+    @FXML
+    private Button ArtistAddButton;
+    @FXML
+    private Button ArtistClearButton;
+    @FXML
+    private Button ArtistDeleteButton;
+    @FXML
+    private Button ArtistBrowseButton;
+    @FXML
+    private ListView ArtistListView;
+
+    private ArrayList<Artist> artists = new ArrayList<>();
+
 
     public void klasse () {
 
         System.out.println("Ik print nu deze text uit: " + ArtistName.getSelectedText());
         System.out.println(ArtistName.getCharacters());
+    }
+
+    public void addButton(){
+        Artist artist = new Artist(this.ArtistName.getText(), this.ArtistGenre.getText(), this.ArtistInfo.getText());
+        this.artists.add(artist);
+    }
+
+    public void clearButton(){
+        this.ArtistGenre.clear();
+        this.ArtistName.clear();
+        this.ArtistInfo.clear();
+    }
+
+    public void deleteButton(){
+
+        System.out.println(this.artists.toString());
     }
 
 
