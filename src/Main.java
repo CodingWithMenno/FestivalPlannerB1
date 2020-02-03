59717fc967f9ec7306d142bee78cf40439ddb45f
Menno <my.bil@student.avans.nl>
2020-02-03 12:19:32 +0100

    Lege code

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        System.out.println("hoi");
-        System.out.println("doeg");
-        //hallo
+        
     }
 
 
