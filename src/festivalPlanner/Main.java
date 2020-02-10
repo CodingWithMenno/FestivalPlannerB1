ad0d84badf707d285b727b08d6d5dabe34418851
Davy <dj.raitt@student.avans.nl>
2020-02-10 12:15:38 +0100

    Class FileIO aangemaakt. Deze heeft 4 geteste en werkende methodes. 2 om arrays te lezen schrijven 2 om strings te lezen en schrijven Beide gaan via FILEIO

--- src/festivalPlanner/Main.java
+++ src/festivalPlanner/Main.java
@@ -1,8 +1,11 @@
 package festivalPlanner;
 
 import festivalPlanner.gui.StageManager;
+import festivalPlanner.tools.FileIO;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 
 public class Main extends Application {
@@ -11,7 +14,22 @@ public class Main extends Application {
 
         //SceneManager sceneManager = new SceneManager();
 
-        launch(StageManager.class);
+        //launch(StageManager.class);
+        ArrayList<String> testarray = new  ArrayList<String>();
+        testarray.add("Een");
+        testarray.add("Twee");
+        testarray.add("Drie");
+        testarray.add("Vier");
+        testarray.add("Vijf");
+        testarray.add("Zes");
+        testarray.add("Nog een test");
+
+
+        FileIO writer = new FileIO();
+//        writer.writeStringToFile("Dit is nog een test", "/D:/Test/File.txt");
+//        writer.readStringFromFile("/D:/Test/File.txt");
+        writer.writeArrayList(testarray, "/D:/Test/Array.txt");
+        System.out.println(writer.readArrayList("/D:/Test/Array.txt"));
 
     }
 
---
+++ src/festivalPlanner/tools/FileIO.java
@@ -0,0 +1,77 @@
+package festivalPlanner.tools;
+
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class FileIO {
+
+    public void writeStringToFile(String towrite, String path){
+        try {
+            FileWriter writer = new FileWriter(path, true);
+            writer.write(towrite);
+            writer.write("\r\n");   // write new line
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readStringFromFile(String path){
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeArrayList(ArrayList<String> arraylist, String path){
+        try {
+            FileWriter writer = new FileWriter(path, true);
+
+            for (int i = 0; i < arraylist.size(); i++) {
+                writer.write(arraylist.get(i));
+                writer.write("\r\n");   // write new line
+                if (arraylist.size() == (i + 1)) {
+                    writer.write("#");
+                    writer.write("\r\n");
+                }
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> readArrayList(String path){
+        ArrayList<String> returnlist = new ArrayList<String>();
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                    returnlist.add(line);
+                if (line.equals("#")) {
+                    break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    return returnlist;}
+
+
+
+
+}
