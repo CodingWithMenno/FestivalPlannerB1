65e1268f07a3663b915abd849fc08d967c9173f8
Samir <samir.rademakers2@gmail.com>
2020-02-10 14:22:42 +0100

    main aangepast

--- src/festivalPlanner/Main.java
+++ src/festivalPlanner/Main.java
@@ -1,11 +1,8 @@
 package festivalPlanner;
 
 import festivalPlanner.gui.StageManager;
-import festivalPlanner.tools.FileIO;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
-import java.util.ArrayList;
 
 
 public class Main extends Application {
@@ -14,22 +11,7 @@ public class Main extends Application {
 
         //SceneManager sceneManager = new SceneManager();
 
-        //launch(StageManager.class);
-        ArrayList<String> testarray = new  ArrayList<String>();
-        testarray.add("Een");
-        testarray.add("Twee");
-        testarray.add("Drie");
-        testarray.add("Vier");
-        testarray.add("Vijf");
-        testarray.add("Zes");
-        testarray.add("Nog een test");
-
-
-        FileIO writer = new FileIO();
-//        writer.writeStringToFile("Dit is nog een test", "/D:/Test/File.txt");
-//        writer.readStringFromFile("/D:/Test/File.txt");
-        writer.writeArrayList(testarray, "/D:/Test/Array.txt");
-        System.out.println(writer.readArrayList("/D:/Test/Array.txt"));
+        launch(StageManager.class);
 
     }
 
